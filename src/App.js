14bec5b33cb9d9caf79ab01d078b268de67f5108
poorna99122 <poornachandrarao99122@gmail.com>
2021-10-30 15:04:15 -0700

    working with lists

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-
-function Header(props){
+function Header(props) {
   console.log(props);
   return (
     <header>
       <h1>{props.Hname}'s kitchen</h1>
     </header>
-  )
+  );
 }
 
-
-function Main(Props){
+function Main(props) {
   return (
     <section>
-      <p>We serve the most {Props.adjective} food around</p>
+      <p>We serve the most {props.adjective} food around</p>
+      <ul style={{textAlign:"left"}}>
+        {props.dishes.map((dish) => (
+          <li>{dish}</li>
+        ))}
+      </ul>
     </section>
-  )
+  );
 }
 
-
-function Footer(Props){
+function Footer(props) {
   return (
     <footer>
-      <p>Copyright {Props.year} </p>
+      <p>Copyright {props.year} </p>
     </footer>
-  )
+  );
 }
 
-
-
+const dishes = ["Macaroni and cheese", "Salmon", "Tofu with veg"];
 
 function App() {
   return (
     <div className="App">
-    <Header Hname ="PCR"/>
-    <Main adjective = "Amazing"/>
-    <Footer year = {new Date().getFullYear()}/>
-
+      <Header Hname="PCR" />
+      <Main adjective="Amazing" dishes={dishes} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
