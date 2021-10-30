84a9fcbdcfb2fda33e3bee27ecfbd0d58e2c52b4
poorna99122 <poornachandrarao99122@gmail.com>
2021-10-30 15:36:01 -0700

    adding fragments

--- src/index.js
+++ src/index.js
@@ -4,35 +4,22 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+function AppTwo() {
+  return <h1> This is the second App </h1>;
+}
+
 ReactDOM.render(
-  // React.createElement("h1",{style: {color:"blue"} }, "Heyyy Everyone!"),
-  // React.createElement(
-  //   "ul", 
-  //   null ,
-  //   React.createElement("li", null, "Monday"),
-  //   React.createElement("li", null, "Tuesday"),
-  //   React.createElement("li", null, "Wednesday"),
-  //   React.createElement("li", null, "Thursday"),
-  //   React.createElement("li", null, "Friday"),
-  //   React.createElement("li", null, "Saturday"),
-  //   React.createElement("li", null, "Sunday"),
+  // <React.Fragment>
+  //   <App />
+  //   <AppTwo />
+  // </React.Fragment>,
 
+//   <>
+//   <App />
+//   <AppTwo />
+// </>,
 
-
-  // ),
-
-//Instead of writing like as above we can write by Using JSX like as below
+<App/>,
  
-// <ul>
-//   <li>Monday</li>
-//   <li>Tuesday</li>
-//   <li>Wednesday</li>
-//   <li>Thursday</li>
-//   <li>Friday</li>
-//   <li>Satruday</li>
-//   <li>Sunday</li>
-// </ul>,
-
-<App></App>,
   document.getElementById("root")
 );
